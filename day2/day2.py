86885f994a0e115057742e47bf141c193718c90f
SylvainStak <stackersap@gmail.com>
2021-12-02 05:17:47 +0000

    added day 2 solution

--- day2/day2.py
+++ day2/day2.py
@@ -2,4 +2,44 @@ with open('input.txt') as file:
     input = file.readlines()
     input = [line.rstrip() for line in input]
 
-print(input)
+# Part 1
+horizontal = 0
+depth = 0
+
+for x in input:
+    splitted = x.split(' ')
+    direction = splitted[0]
+    distance = int(splitted[1])
+
+    if direction == 'forward':
+        horizontal += distance
+
+    if direction == 'up':
+        depth -= distance
+
+    if direction == 'down':
+        depth += distance
+
+print(horizontal*depth)
+
+# Part 2
+horizontal = 0
+depth = 0
+aim = 0
+
+for x in input:
+    splitted = x.split(' ')
+    direction = splitted[0]
+    distance = int(splitted[1])
+
+    if direction == 'forward':
+        horizontal += distance
+        depth += aim*distance
+
+    if direction == 'up':
+        aim -= distance
+
+    if direction == 'down':
+        aim += distance
+
+print(horizontal*depth)
